d15a8153d477a56a41be1c63e495d6a3dea985d6
Numseed <160110802+Numseed@users.noreply.github.com>
2024-02-15 16:11:23 +0800

    Add files via upload

---
+++ For_my_bea.py
@@ -0,0 +1,31 @@
+﻿import requests 
+import streamlit as st 
+
+st.set_page_config(page_title="To my Loving Bea", page_icon="😍", layout="wide")
+
+# ---- HEADER SECTION ----
+with st.container():
+    st.subheader("Dear, Bea my Love 🥰")
+st.title("💌Happy Valentines Day to you💌")
+st.write("I just want to wish you a happy birthday, I love you very much")
+new_varnew_var = st.write("This is my website created just for you")
+
+#---- My Letter to You ----
+with st.container():
+    st.write("---")
+    st.header ("Well isa toh sa mga regalo ko sayo dapat")
+    st.write("##")
+    st.write("Hello, hehehe my love, I just wanted to say Happy Valentines to you, and I love you very much.")
+    st.write("It has been a year since we have been together, and this is the second time we are celebrating our Valentines")
+    st.write("Hoping for us to celebrate more events like this together, and hope na eventhough we will be in college, we will still be together")
+    st.write("Well last year was very rough, ang dami nating pinag-awayan, dami nating pinag dadaanan sa school man o sa pamilya, pero you were always there for me, and I am Thankful for that")
+    st.write("I hope na sa pagtagal we will love each other more and more kaya sana ipagpatuloy mo lang ang pagiging ikaw at ipag atuloy mo lang ang pag mamahala sa akin kasi hindi kita titigila")
+    st.write("LOVE YOU A LOT - AXL (your bebe)")
+
+with st.container():
+    st.write("---")
+    st.header ("Sorry kung medyo nalate")
+    st.write ("I had some problems, dapat may asset toh na pusa pero ayaw nyang gumana so for now ito muna hehehe")
+   
+   
+   
